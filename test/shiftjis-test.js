590b57466a90fa9f083f191aaf64ccfb33097bce
eirblaze <eirblaze@live.jp>
2020-10-15 19:19:17 +0900

    テスト作成中

--- test/shiftjis-test.js
+++ test/shiftjis-test.js
@@ -42,4 +42,46 @@ describe("ShiftJIS tests #node-web", function () {
         assert.strictEqual(iconv.decode(utils.bytes("87 40"), "shiftjis"), "①");
         assert.strictEqual(utils.hex(iconv.encode("①", "shiftjis")), "87 40");
     });
+
+    it("ShiftJIS correctly character reference encoded/decoded", function () {
+        const testString = "日本abcｱｲｳ🎈", //unicode contains ShiftJIS-code and ascii
+            testStringBig5Buffer = utils.bytes("93 fa 96 7b 61 62 63 b1 b2 b3 3f "),
+            testStringBig5BufferHtml = utils.bytes(
+                "93 fa 96 7b 61 62 63 b1 b2 b3 26 23 78 31 66 33 38 38 3b"
+            ),
+            testStringBig5BufferCss = utils.bytes(
+                "93 fa 96 7b 61 62 63 b1 b2 b3 26 23 78 31 66 33 38 38 3b"
+            ),
+            testString2 = "試ｽ～🍙",
+            testStringBig5Buffer2 = utils.bytes("8e 8e bd 3f"),
+            testStringBig5Buffer2Html = utils.bytes("8e 8e bd 3f"),
+            testStringBig5Buffer2Css = utils.bytes("8e 8e bd 3f");
+
+        assert.strictEqual(
+            utils.hex(iconv.encode(testString, "shiftjis", {})),
+            utils.hex(testStringBig5Buffer)
+        );
+        assert.strictEqual(
+            utils.hex(iconv.encode(testString2, "shiftjis", {})),
+            utils.hex(testStringBig5Buffer2)
+        );
+
+        assert.strictEqual(
+            utils.hex(iconv.encode(testString, "shiftjis", { charRefMode: "html" })),
+            utils.hex(testStringBig5BufferHtml)
+        );
+        assert.strictEqual(
+            utils.hex(iconv.encode(testString2, "shiftjis", { charRefMode: "html" })),
+            utils.hex(testStringBig5Buffer2Html)
+        );
+
+        assert.strictEqual(
+            utils.hex(iconv.encode(testString, "shiftjis", { charRefMode: "css" })),
+            utils.hex(testStringBig5BufferCss)
+        );
+        assert.strictEqual(
+            utils.hex(iconv.encode(testString2, "shiftjis", { charRefMode: "css" })),
+            utils.hex(testStringBig5Buffer2Css)
+        );
+    });
 });
